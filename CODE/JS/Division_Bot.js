a30548f7a0cd1906c64ed9fc8536cb09fe6fa538
채종우 <cjw200525@gmail.com>
2018-08-30 10:10:49 +0900

    20180830 - 1000
    디비전 봇 2.1.1
    > 트윅
    > 상태 명령어 추가
    > 쓸모없던 함수 뺌

--- CODE/JS/Division_Bot.js
+++ CODE/JS/Division_Bot.js
@@ -1,127 +1,114 @@
 /*
 KakaoTalk_Division_BOT
-By NOPE & KOR_Di
+By KOR_Di
 
-version 2.1.0
+version 2.1.1
 
 안녕하세요! [[보낸사람]] 님!
 명령어 모음집 입니다! 
-너무 반복적으로 자주 사용하시면 밴망치 맞아요![[다음채팅]]=더 디비전 관련=
+너무 반복적으로 자주 사용하시면 밴망치 맞아요!
+
+=더 디비전 관련=
 !규칙
-KOR_Di BOT >> 오픈톡방 규칙을 불러옵니다.
+>> 오픈톡방 규칙을 불러옵니다.
 
 !상점정보 
-KOR_Di BOT >> 디비전 최신 상점정보를 불러옵니다.
+>> 디비전 최신 상점정보를 불러옵니다.
 
 !필보지도 
-KOR_Di BOT >> 필드보스 주소를 불러옵니다.
+>> 필드보스 주소를 불러옵니다.
 
 !딜연산
-KOR_Di BOT >> 딜연산 주소를 불러옵니다.[[다음채팅]]=커뮤니티 관련=
+>> 딜연산 주소를 불러옵니다.
+
+=커뮤니티 관련=
 !디코
-KOR_Di BOT >> 디스코드 초대링크를 불러옵니다.
+>> 디스코드 초대링크를 불러옵니다.
 
 !카페
-KOR_Di BOT >> 카페링크를 불러옵니다.[[다음채팅]]=파티 관련=
+>> 카페링크를 불러옵니다.
+
+=파티 관련=
 !파티
-NOPE BOT >> 파티 명령어 도움말을 불러옵니다.
+>> 파티 명령어 도움말을 불러옵니다.
 
-설명
-공백 두칸이 기준입니다.
+===============================================
 
-/파티c [파티제목]  [파티시작시간]  [파티설명]	- 파티만들기
+KakaoTalk_Party_BOT
+By NOPE
 
-/파티j [파티제목]	[슬롯번호]				- 파티참가하기
+안녕하세요! [[보낸사람]] 님!
+파티 매니저 봇 입니다!
+너무 반복적으로 자주 사용하시면 밴망치 맞아요!
 
-/파티d [파티제목]						- 파티삭제하기
+공백 두번을 기준으로 합니다!
 
-/파티e [파티제목]						- 파티나가기
+/파티c [파티제목]  [파티시작시간]  [파티설명]
+>> 파티를 생성합니다.
 
-/파티x [파티제목]						- 파티정보
+/파티j [파티제목]  [슬롯번호]
+>> 파티슬롯에 참가합니다
 
-/파티l								- 파티리스트
+/파티d [파티제목]
+>> 파티를 삭제합니다.
+
+/파티e [파티제목]
+>> 파티를 나갑니다.
+
+/파티x [파티제목]
+>> 파티정보를 불러옵니다.
+
+/파티l
+>> 파티리스트를 불러옵니다.
+
+/상태
+>> 봇 정보를 불러옵니다.
 */
 
 var PartyList = {};
 var PartyMember = {};
 const INIT = ["비어있음", "비어있음", "비어있음", "예비", "예비"];
 
-function isExistParty(name) {
-	
+function isExistParty(name) {	// 파티리스트에 파티가 있는지 없는지 확인
+
 	if(name in PartyList) {
-		
-		return true;
-		
+
+		return true;			// 있으면 true
+
 	} else {
-		
-		return false;
-		
+
+		return false;			// 없으면 false
+
 	}
-	
+
 }
 
-function CountUser(name) {
-	
-	if(isExistParty(name)) {
-	
-		return PartyMember[name][0];
-	
+function isExistMember(name, user) {			// 파티내 유저가 존재하는지 확인
+
+	if(user in PartyMember[name]) {
+
+		return true;							// 있으면 true 
+
+	} else {
+
+		return false;							// 없으면 false
+
 	}
-	
+
 }
 
-function isExistSlot(name, user, slotnum) {
-	
-	if(isExistParty(name)) {
-		
-		if(user == PartyMember[name][slotnum]) {
-		
-			return true;
-		
-		} else {
-		
-			return false;
-		
-		}
-		
-	}
-	
-}
+function isLeader(name, user) {					// 리더인지 아닌지 확인	
 
-function isExistMember(name, user) {
-	
-	if(isExistParty(name)) {
-		
-		if(user in PartyMember[name]) {
-		
-			return true;
-		
-		} else {
-		
-			return false;
-		
-		}
-		
-	}
-	
-}
+	if(user == PartyList[name][2]) {
 
-function isLeader(name, user) {
-	
-	if(isExistParty(name)) {
-		
-		if(user == PartyList[name][2]) {
-		
-			return true;
-		
-		} else {
-		
-			return false;
-		
-		}
-		
+		return true;							// 맞으면 true
+
+	} else {
+
+		return false;							// 아니면 false
+
 	}
-	
+
 }
 
 function crea(name, time, discription, user, replier) {
@@ -132,7 +119,7 @@ function crea(name, time, discription, user, replier) {
 		PartyMember[name] = [1, user, "비어있음", "비어있음", "비어있음", "예비", "예비"];
 
 		replier.reply("==파티생성완료==\n파티제목 - " + name + "\n파티시작시간 - " + PartyList[name][0] + "\n파티설명 - " + PartyList[name][1] + "\n==============\n리더. " + PartyList[name][2]);
-		
+
 	} else {
 
 		replier.reply("==파티생성실패==\n이미 있는 파티입니다!");
@@ -146,26 +133,27 @@ function dele(name, user, replier) {
 	if(isExistParty(name)) {
 
 		if(isLeader(name, user)) {
-	
+
 			delete PartyList[name];
 			delete PartyMember[name];
 			replier.reply("==파티삭제완료==");
 
 		} else {
-			
+
 			replier.reply("==파티삭제실패==\n리더가 아닙니다!");
-			
+
 		}
-		
+
 	} else {
-		
+
 		replier.reply("==파티삭제실패==\n없는 파티 입니다!");
-		
+
 	}
-	
+
 }
 
 function join(name, slotnum, user, replier) {
+	
 	slotnum++;
 	
 	if(isExistParty(name)) {
@@ -174,9 +162,9 @@ function join(name, slotnum, user, replier) {
 			
 			if(0<slotnum) {
 			
-				if(!isExistSlot(name, user, slotnum) || !isExistMember(name, user)) {
+				if(!isExistMember(name, user)) {
 				
-					if(CountUser(name) < 7) {
+					if(PartyMember[name][0] < 7) {
 					
 						PartyMember[name][0]++;
 						PartyMember[name][slotnum] = user;
@@ -191,7 +179,7 @@ function join(name, slotnum, user, replier) {
 				} else {
 					
 					replier.reply("==파티참가실패==\n이미 파티에 있습니다!");
-					
+
 				}
 	
 			} else {
@@ -279,6 +267,16 @@ function exte(name, replier) {
 	
 }
 
+function info(sender, replier) {
+	
+	var infoJs = Bot.getJsVersion();
+	var infoDeviceBL = Device.getBatteryLevel();
+	var infoDeviceBT = Device.getBatteryTemp();
+	
+	replier.reply("==BOT DEBUG MODE==\nJava Script: " + infoJs + "\nBOT BatteryLevel: " + infoDeviceBL + "\nBOT BatteryTemp: " + infoDeviceBT + "\n==================\nDict::PartyList - " + PartyList + "\n==================\nDict::PartyMember - " + PartyMember);
+	
+}
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB) {
 	
 	if(room == "TEST0525") {
@@ -310,6 +308,10 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
 			case "/파티l":
 				list(replier);										// 파티l						- 파티리스트
 			break;	
+			
+			case "/정보":
+				info(replier);										// 파티l						- 파티리스트
+			break;
 			
 		}
 		
